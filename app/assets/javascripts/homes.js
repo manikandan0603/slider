0df7f44751f18334a83534e77b7dd3d067bb5ec3
manikandan0603 <manikandan0603@gmail.com>
2016-04-26 03:53:15 +0530

    second commit

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,6 +1,16 @@
 $(function() {
 
 		var ivalue = $('#slider').attr("data-slider-value")
+		var s1 = $('#slider').attr("data-s1")
+		var s2 = $('#slider').attr("data-s2")
+		var s3 = $('#slider').attr("data-s3")
+		var s4 = $('#slider').attr("data-s4")
+		var s5 = $('#slider').attr("data-s5")
+		var s6 = $('#slider').attr("data-s6")
+		var s7 = $('#slider').attr("data-s7")
+		var s8 = $('#slider').attr("data-s8")
+		var s9 = $('#slider').attr("data-s9")
+		var s10 = $('#slider').attr("data-s10")
 	  $( "#slider" ).slider({
 	  min: 1,
 	  max: 10,
@@ -11,7 +21,41 @@ $(function() {
 		  	var sub_slider_values = []
 		    $("#create-pane").append("<h1>"+i+"</h1>");
 		    $("#create-pane").append("<div id='new-slider-"+i+"'></div>");
-		    $('#new-slider-'+i).slider({range: true});
+		    $('#new-slider-'+i).slider({range: true, 
+		    	if (i = "1") {
+		    		value: s1
+		    	}
+		    	else if (i = "2") {
+		    		value: s2
+		    	}
+		    	else if (i = "3") {
+		    		value: s3
+		    	}
+		    	else if (i = "4") {
+		    		value: s4
+		    	}
+		    	else if (i = "5") {
+		    		value: s5
+		    	}
+		    	else if (i = "6") {
+		    		value: s6
+		    	}
+		    	else if (i = "7") {
+		    		value: s7
+		    	}
+		    	else if (i = "8") {
+		    		value: s8
+		    	}
+		    	else if (i = "9") {
+		    		value: s9
+		    	}
+		    	else if (i = "10") {
+		    		value: s10
+		    	}
+		    	else{
+
+		    	}
+		    });
 		   }
 
 	  $('#slider').on("slidechange", function(event, ui){
